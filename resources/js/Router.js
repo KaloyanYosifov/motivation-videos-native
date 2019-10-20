b82dae4e3ce5c2d7210ce05a4c37c9d1b946cadf
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-10-20 21:00:41 +0300

    Create a more complex navigator

--- resources/js/Router.js
+++ resources/js/Router.js
@@ -3,20 +3,40 @@
  */
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
 
 /**
  * Internal dependencies.
  */
 import Home from '@/pages/Home';
+import Login from '@/pages/Login';
 import Motivations from '@/pages/Motivations';
 
-const Router = createBottomTabNavigator({
+const guestRoutes = createStackNavigator({
+    Login: {
+        screen: Login,
+    },
+});
+
+const loggedInRoutes = createBottomTabNavigator({
     Home: {
         screen: Home,
     },
     Motivations: {
         screen: Motivations,
     },
+});
+
+const Router = createStackNavigator({
+    Guest: {
+        screen: guestRoutes,
+    },
+    LoggedIn: {
+        screen: loggedInRoutes,
+        name: 'Register',
+    },
+}, {
+    headerMode: 'none',
 });
 
 export default createAppContainer(Router);
---
+++ resources/js/pages/Login.js
@@ -0,0 +1,23 @@
+/**
+ * External dependencies.
+ */
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+/**
+ * Internal dependencies.
+ */
+
+class Login extends React.Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text>
+                    Login
+                </Text>
+            </SafeAreaView>
+        );
+    }
+}
+
+export default Login;

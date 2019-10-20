56e54cedb9c87056e132306599a1753fd9c000e9
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-10-20 18:38:32 +0300

    Fix indents, remove old test and add new structure for the app

---
+++ resources/js/App.jsx
@@ -0,0 +1,21 @@
+/**
+ * External dependencies.
+ */
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+/**
+ * Internal dependencies.
+ */
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <Text>
+                Hello
+            </Text>
+        </SafeAreaView>
+    );
+};
+
+export default App;

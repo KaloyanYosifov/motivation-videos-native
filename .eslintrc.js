f096eb4b7df82b40e37e35e78996593b8d59e59d
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-10-20 18:29:29 +0300

    Fix app name and remove prettier from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 0,
+    'babel/semi': 1,
+    indent: [
+      'error',
+      4
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ]
+  }
 };

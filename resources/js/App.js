04153f0574bf9c608a4676ee6d765ea418fbd772
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-10-20 18:40:15 +0300

    Fix extension

---
+++ resources/js/App.js
@@ -0,0 +1,23 @@
+/**
+ * External dependencies.
+ */
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+/**
+ * Internal dependencies.
+ */
+
+class App extends React.Component {
+    render() {
+        return (
+            <SafeAreaView>
+                <Text>
+                    Hello
+                </Text>
+            </SafeAreaView>
+        );
+    }
+}
+
+export default App;

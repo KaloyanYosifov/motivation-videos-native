13c710bd32ea8e1b58973c1f14c130745f60d1d7
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-10-20 18:32:29 +0300

    Add editorconfig and reconfigure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  rules: {
-    'prettier/prettier': 0,
-    'babel/semi': 1,
-    indent: [
-      'error',
-      4
-    ],
-    quotes: [
-      'error',
-      'single'
-    ],
-    semi: [
-      'error',
-      'always'
-    ]
-  }
+    root: true,
+    extends: '@react-native-community',
+    rules: {
+        'prettier/prettier': 0,
+        indent: [
+            'error',
+            4
+        ],
+        quotes: [
+            'error',
+            'single'
+        ],
+        semi: [
+            'error',
+            'always'
+        ]
+    }
 };

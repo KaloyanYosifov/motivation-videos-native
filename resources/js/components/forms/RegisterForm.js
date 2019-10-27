6b42f2a9c1f1d5f6a33310455bf41bb41b3cd40b
Kaloyan Yosifov <kaloqn665@gmail.com>
2019-10-27 20:40:29 +0200

    Add register form submission

--- resources/js/components/forms/RegisterForm.js
+++ resources/js/components/forms/RegisterForm.js
@@ -2,7 +2,7 @@
  * External dependencies.
  */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
 
 /**
  * Internal dependencies.
@@ -11,54 +11,87 @@ import Button from '@/components/Button';
 import Navigator from '@/classes/Navigator';
 import Input from '@/components/form-elements/Input';
 import BaseFormStyles from '@/styles/BaseFormStyles';
+import api from '@/utils/api';
 
 class RegisterForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+        };
+    }
+
+    handleSubmit = () => {
+        const { name, email, password } = this.state;
+        console.log(this.state);
+        api.post('register', {
+            name,
+            email,
+            password,
+        })
+            .then(response => {
+                console.log(response);
+            });
+    }
+
     render() {
         return (
-            <View style={this.props.style}>
-                <Text style={BaseFormStyles.title}>
-                    Register
-                </Text>
+            <ScrollView style={this.props.style}>
+                <View>
 
-                <View style={BaseFormStyles.inputsContainer}>
-                    <Input
-                        style={BaseFormStyles.input}
-                        label="Name:"
-                        inputProps={{
-                            autoCompleteType: 'name',
-                        }}
-                    />
-
-                    <Input
-                        style={BaseFormStyles.input}
-                        label="Email:"
-                        inputProps={{
-                            autoCompleteType: 'email',
-                        }}
-                    />
-
-                    <Input
-                        style={BaseFormStyles.input}
-                        label="Password:"
-                        inputProps={{
-                            secureTextEntry: true,
-                            autoCompleteType: 'password',
-                            textContentType: 'password',
-                        }}
-                    />
-                </View>
-
-                <View style={BaseFormStyles.buttonContainer}>
-                    <Button style={BaseFormStyles.button} text="Register" />
-
-                    <Text
-                        style={BaseFormStyles.additionalText}
-                        onPress={() => Navigator.navigate('Login')}
-                    >
-                        Have an account?
+                    <Text style={BaseFormStyles.title}>
+                        Register
                     </Text>
+
+                    <View style={BaseFormStyles.inputsContainer}>
+                        <Input
+                            style={BaseFormStyles.input}
+                            label="Name:"
+                            inputProps={{
+                                autoCompleteType: 'name',
+                                onChangeText: name => this.setState({ name }),
+                                autoCapitalize: 'none',
+                            }}
+                        />
+
+                        <Input
+                            style={BaseFormStyles.input}
+                            label="Email:"
+                            inputProps={{
+                                autoCompleteType: 'email',
+                                onChangeText: email => this.setState({ email }),
+                                autoCapitalize: 'none',
+                            }}
+                        />
+
+                        <Input
+                            style={BaseFormStyles.input}
+                            label="Password:"
+                            inputProps={{
+                                secureTextEntry: true,
+                                autoCompleteType: 'password',
+                                textContentType: 'password',
+                                onChangeText: password => this.setState({ password }),
+                                autoCapitalize: 'none',
+                            }}
+                        />
+                    </View>
+
+                    <View style={BaseFormStyles.buttonContainer}>
+                        <Button style={BaseFormStyles.button} text="Register" onPress={this.handleSubmit} />
+
+                        <Text
+                            style={BaseFormStyles.additionalText}
+                            onPress={() => Navigator.navigate('Login')}
+                        >
+                            Have an account?
+                        </Text>
+                    </View>
                 </View>
-            </View>
+            </ScrollView>
         );
     }
 }

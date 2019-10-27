14bd06ee79600ede6b86c8ebeea3d699c8e42fd5
KaloyanYosifov <kaloqn665@gmail.com>
2019-10-27 21:07:49 +0200

    Make margin smaller for logo

--- resources/js/styles/LoginRegisterStyles.js
+++ resources/js/styles/LoginRegisterStyles.js
@@ -5,6 +5,6 @@ import { ScaledSheet } from 'react-native-size-matters';
 
 export default ScaledSheet.create({
     logo: {
-        marginBottom: '80@ms',
+        marginBottom: '40@ms',
     },
 });
